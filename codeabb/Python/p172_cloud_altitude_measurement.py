cd29df8f7ea23e6b7ddcc61ca4327a55f620dd12
Raul <raul@whoisrgj.com>
2016-03-05 18:30:11 -0600

    added p172

---
+++ codeabb/Python/p172_cloud_altitude_measurement.py
@@ -0,0 +1,20 @@
+"""
+CodeAbbey, Problem 172
+Coded by whoisrgj
+"""
+import math
+
+
+def cloud_altitude(d1, a, b):
+    tan_a = math.tan(math.radians(a))
+    tan_b = math.tan(math.radians(b))
+    d2 = (tan_a * d1) / (tan_b - tan_a)
+    return round(tan_b * d2)
+
+
+def main():
+    n = int(input())
+    print(*(cloud_altitude(d1, a, b) for d1, a, b in (map(float, input().split()) for _ in range(n))))
+
+if __name__ == '__main__':
+    main()
